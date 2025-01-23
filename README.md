6c12340c76d44ec7276b7523f9675afacb2ae852
Jose <jos.rojas97@hotmail.com>
2025-01-23 17:30:58 -0500

    Implementación de código JS para funcionamiento amigo secreto

---
+++ README.md
@@ -0,0 +1,13 @@
+# Proyecto: Amigo Secreto 
+
+## Descripción 📋
+Este proyecto es una aplicación para organizar un juego de "Amigo Secreto". Permite a los usuarios escribir nombres y añadirlos a una lista que se usará para sortear el amigo secreto.
+
+## Características 🔎
+- Registro de participantes
+- Asignación aleatoria de amigos secretos y validación de entrada
+- Muestra de resultado amigo secreto
+
+## Preview ▶️
+
+![image](https://github.com/user-attachments/assets/8671aa54-28bb-4f4f-870a-7ac8bce57b83)
--- app.js
+++ app.js
@@ -1,1 +1,35 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+/* Función para agregar nombres y limpiar el campo de entrada*/
+function agregarAmigo() {
+    const input = document.getElementById('amigo');
+    const nombre = input.value.trim();
+    if (nombre) {
+        amigos.push(nombre);
+        actualizarLista();
+        input.value = '';
+    }
+}
+
+/* Función para mostrar los nombres agregados en lista */
+function actualizarLista() {
+    const lista = document.getElementById('listaAmigos');
+    lista.innerHTML = '';
+    amigos.forEach((amigo) => {
+        const li = document.createElement('li');
+        li.textContent = amigo;
+        lista.appendChild(li);
+    });
+}
+
+/* Función para sortear los nombres y validar la entrada */
+function sortearAmigo() {
+    if (amigos.length === 0) {
+        alert('Por favor, inserte un nombre.');
+        return;
+    }
+    const indiceSorteado = Math.floor(Math.random() * amigos.length);
+    const amigoSorteado = amigos[indiceSorteado];
+    const resultado = document.getElementById('resultado');
+    resultado.innerHTML = `<li>El amigo secreto es: ${amigoSorteado}</li>`;
+}
